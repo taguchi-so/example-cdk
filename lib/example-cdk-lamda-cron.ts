d0ae26719c7bd27fbdde19913a26955288447f26
taguchi-so <katsuya.taguchi@b.so-tech.co.jp>
2019-11-25 14:51:19 +0900

    Added vpc, subnet, securitygroup to lamda

--- lib/example-cdk-lamda-cron.ts
+++ lib/example-cdk-lamda-cron.ts
@@ -4,11 +4,19 @@ import * as s3 from "@aws-cdk/aws-s3";
 import * as iam from "@aws-cdk/aws-iam";
 import * as events from "@aws-cdk/aws-events";
 import * as targets from "@aws-cdk/aws-events-targets";
+import * as ec2 from "@aws-cdk/aws-ec2";
 
-import { RemovalPolicy } from "@aws-cdk/core";
+interface subnetCongfig {
+  subnetId: string;
+  availabilityZone: string;
+  routeTableId: string;
+}
 
 interface StageContext {
   bucketName: string;
+  vpcName: string;
+  privateSubnets: subnetCongfig[];
+  securityGroupID: string;
 }
 export class ExampleCdkLambdaCronStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -17,7 +25,12 @@ export class ExampleCdkLambdaCronStack extends cdk.Stack {
     const env: string = this.node.tryGetContext("env") || "dev";
     const revision: string = this.node.tryGetContext("revision") || "";
     const context: StageContext = this.node.tryGetContext(env) || {};
-    if (context.bucketName == "") {
+
+    if (
+      context.bucketName == "" ||
+      context.vpcName == "" ||
+      context.privateSubnets.length == 0
+    ) {
       throw new Error(
         `error: invalid context:${JSON.stringify({
           env,
@@ -26,6 +39,8 @@ export class ExampleCdkLambdaCronStack extends cdk.Stack {
         })}`
       );
     }
+
+    // create s3 bucket
     const bucket = new s3.Bucket(this, "Bucket", {
       versioned: false,
       bucketName: context.bucketName,
@@ -33,7 +48,6 @@ export class ExampleCdkLambdaCronStack extends cdk.Stack {
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
       removalPolicy: cdk.RemovalPolicy.DESTROY
     });
-
     const getPolicyStatement = new iam.PolicyStatement();
     getPolicyStatement.addActions("s3:GetObject");
     getPolicyStatement.addResources(`arn:aws:s3:::${context.bucketName}/*`);
@@ -49,6 +63,28 @@ export class ExampleCdkLambdaCronStack extends cdk.Stack {
     });
     bucket.addToResourcePolicy(putPolicyStatement);
 
+    // describe vpc and private subnet and security group
+    const vpc = ec2.Vpc.fromLookup(this, "VPC", {
+      vpcId: context.vpcName
+    });
+
+    const selectSubnets: ec2.SelectedSubnets = vpc.selectSubnets({
+      subnets: context.privateSubnets.map(privateSubnet => {
+        return ec2.Subnet.fromSubnetAttributes(this, privateSubnet.subnetId, {
+          subnetId: privateSubnet.subnetId,
+          availabilityZone: privateSubnet.availabilityZone,
+          routeTableId: privateSubnet.routeTableId
+        });
+      })
+    });
+
+    const securityGroup = ec2.SecurityGroup.fromSecurityGroupId(
+      this,
+      "SecurityGroup",
+      context.securityGroupID
+    );
+
+    // create lamda to selected vpc and private subnet
     const cronLambda = new lambda.Function(this, "cronLambda", {
       code: lambda.Code.asset("src/lambda/sample"),
       handler: "cron.handler",
@@ -57,8 +93,12 @@ export class ExampleCdkLambdaCronStack extends cdk.Stack {
       environment: {
         ENV: env,
         REVISION: revision
-      }
+      },
+      vpc: vpc,
+      vpcSubnets: selectSubnets,
+      securityGroup: securityGroup
     });
+
     const rule = new events.Rule(this, "Rule", {
       schedule: events.Schedule.expression("cron(0 18 ? * MON-FRI *)")
     });
